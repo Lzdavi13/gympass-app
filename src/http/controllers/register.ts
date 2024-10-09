ecb5af3f4ad880514fce9e0d368209a37838ea2a
unknown <lzdavi1325@gmail.com>
2024-10-08 22:54:01 -0300

    feat: handles errors from the user register use case

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 import { RegisterUseCase } from "@/use-cases/register";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
@@ -18,9 +19,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password });
   } catch (err) {
-    return reply.status(409).send({
-      message: err.message,
-    });
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: err.message,
+      });
+    }
+
+    return reply.status(500).send(); // TODO: fix me
   }
 
   return reply.status(201).send();
